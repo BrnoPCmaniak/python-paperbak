e6e763666151cde82170434db8b47ded72b630af
Filip Dobrovolny <brnopcman@gmail.com>
2017-04-24 00:41:35 +0200

    Add constants module

---
+++ python/constants.py
@@ -0,0 +1,8 @@
+"""Constatns for python-paperbak."""
+
+NDATA = 90  # Number of data bytes in a block
+NDOT = 32  # Block X and Y size, dots
+
+# Special address
+MAXSIZE = 0x0FFFFF80  # Maximal (theoretical) length of file
+SUPERBLOCK = 0xFFFFFFFF  # Address of superblock
