4b766e224a427d40f63f623777cabf152c86aa2b
Filip Dobrovolny <brnopcman@gmail.com>
2017-04-30 17:34:24 +0200

    Add test test_dtypes.TestFileTime.test_tobytes

--- python/paperbak/test/test_dtypes.py
+++ python/paperbak/test/test_dtypes.py
@@ -12,6 +12,12 @@ class TestFileTime(unittest.TestCase):
     def test_get_datetime(self):
         """Test that FileTime(0) equals to 1/1/1601"""
         self.assertEqual(FileTime(0).get_datetime(), datetime(1601, 1, 1, tzinfo=timezone.utc))
+    def test_tobytes(self):
+        """Test that converts mateches to new_cpp/test_file_time.cpp."""
+        data = bytes([0x00, 0xc0, 0xb0, 0xfe, 0xc1, 0x63, 0xd2, 0x01])
+        date = datetime.fromtimestamp(1483228800)
+        self.assertEqual(FileTime(date).tobytes(), data)
+
 
 if __name__ == '__main__':
     unittest.main()
