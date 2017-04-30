e23ed6fe43ae6baac8b0433e1850c14400765585
Filip Dobrovolny <brnopcman@gmail.com>
2017-04-30 17:36:37 +0200

    Beautify test_crc16 and test_dtypes

--- python/paperbak/test/test_crc16.py
+++ python/paperbak/test/test_crc16.py
@@ -6,6 +6,7 @@ from paperbak import crc16
 
 from . import TEST_DATA
 
+
 class TestCRC16(unittest.TestCase):
     def test(self):
         """Test CRC16 over TEST_DATA matches new_cpp/test_crc16.cpp."""
@@ -14,6 +15,7 @@ class TestCRC16(unittest.TestCase):
     def test_type(self):
         """Test CRC16 return correct data type."""
         self.assertEqual(type(crc16.crc16(bytes(TEST_DATA))), np.uint16)
+
 
 if __name__ == '__main__':
     unittest.main()
--- python/paperbak/test/test_dtypes.py
+++ python/paperbak/test/test_dtypes.py
@@ -7,11 +7,13 @@ from paperbak.dtypes import FileTime
 class TestFileTime(unittest.TestCase):
     def test_new(self):
         """Test that 1/1/1970 equals to FileTime.EPOCH_DIFF*FileTime.N100_NSEC_IN_SEC."""
-        self.assertEqual(FileTime(datetime(1970,1,1, tzinfo=timezone.utc)),
-                         FileTime.EPOCH_DIFF*FileTime.N100_NSEC_IN_SEC)
+        self.assertEqual(FileTime(datetime(1970, 1, 1, tzinfo=timezone.utc)),
+                         FileTime.EPOCH_DIFF * FileTime.N100_NSEC_IN_SEC)
+
     def test_get_datetime(self):
         """Test that FileTime(0) equals to 1/1/1601"""
         self.assertEqual(FileTime(0).get_datetime(), datetime(1601, 1, 1, tzinfo=timezone.utc))
+
     def test_tobytes(self):
         """Test that converts mateches to new_cpp/test_file_time.cpp."""
         data = bytes([0x00, 0xc0, 0xb0, 0xfe, 0xc1, 0x63, 0xd2, 0x01])
