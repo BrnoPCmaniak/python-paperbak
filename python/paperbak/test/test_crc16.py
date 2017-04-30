abd8055e53727057e3b0ec96262299969403e0ab
Filip Dobrovolny <brnopcman@gmail.com>
2017-04-30 16:41:56 +0200

    Add tests for CRC16

---
+++ python/paperbak/test/test_crc16.py
@@ -0,0 +1,19 @@
+import unittest
+
+import numpy as np
+
+from paperbak import crc16
+
+from . import TEST_DATA
+
+class TestCRC16(unittest.TestCase):
+    def test(self):
+        """Test CRC16 over TEST_DATA is correct."""
+        self.assertEqual(crc16.crc16(bytes(TEST_DATA)), np.uint16(62557))
+
+    def test_type(self):
+        """Test CRC16 return correct data type."""
+        self.assertEqual(type(crc16.crc16(bytes(TEST_DATA))), np.uint16)
+
+if __name__ == '__main__':
+    unittest.main()
